a7f8b1f0906720320cee14c3f40b651e07c8765c
Takaki <takaki55730317@gmail.com>
2019-08-20 17:49:32 +0900

    modify timeout

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
-timeout 100
+timeout 300
 
 # use correct Gemfile on restarts
 before_exec do |server|
